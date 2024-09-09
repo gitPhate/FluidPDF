33db5548cf1191b7e34bd7334d5d6ef7cac9890a
gitPhate <gabriele.ricci.33.95@gmail.com>
2024-09-09 12:23:59 +0200

    fixed array not working in model of pdf

--- src/FluidPDF/Fluid/FluidTemplateOptions.cs
+++ src/FluidPDF/Fluid/FluidTemplateOptions.cs
@@ -2,6 +2,7 @@
 using Fluid.Values;
 using System;
 using System.Data;
+using System.Linq;
 using System.Text.Json.Nodes;
 
 namespace FluidPDF.Fluid
@@ -35,10 +36,15 @@ namespace FluidPDF.Fluid
                         return row[fieldName];
                     }
                 );
-
+            templateOptions.MemberAccessStrategy.Register<JsonValue, object>((src, name) => src[name]!);
+            templateOptions.MemberAccessStrategy.Register<JsonObject, object>((src, name) => src[name]!);
+            templateOptions.MemberAccessStrategy.Register<JsonArray, object>((src, name) => src[name]!);
             templateOptions.MemberAccessStrategy.Register<JsonNode, object>((src, name) => src[name]!);
 
+            templateOptions.ValueConverters.Add(x => x is JsonArray o ? new ArrayValue(o.Select(x => new ObjectValue(x)).ToArray()) : null);
             templateOptions.ValueConverters.Add(x => x is JsonNode o ? new ObjectValue(o) : null);
+            templateOptions.ValueConverters.Add(x => x is JsonValue o ? new ObjectValue(o) : null);
+            templateOptions.ValueConverters.Add(x => x is JsonObject o ? new ObjectValue(o) : null);            
 
             return templateOptions;
         }
