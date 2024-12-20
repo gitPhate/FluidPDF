8bdf2cf52614a6865a98f9f691fb36f414a60425
gitPhate <gabriele.ricci.33.95@gmail.com>
2024-12-20 17:49:49 +0100

    added C#9 support for records in .NET standard

---
+++ src/CSharp9Support/NullableExtensions.cs
@@ -0,0 +1,92 @@
+﻿#if !NETSTANDARD2_1
+
+#nullable disable
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Diagnostics.CodeAnalysis
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property, Inherited = false)]
+    internal sealed partial class AllowNullAttribute : Attribute
+    {
+        public AllowNullAttribute() { }
+    }
+
+    [AttributeUsageAttribute(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property, Inherited = false)]
+    internal sealed partial class DisallowNullAttribute : Attribute
+    {
+        public DisallowNullAttribute() { }
+    }
+
+    [AttributeUsageAttribute(AttributeTargets.Method, Inherited = false)]
+    internal sealed partial class DoesNotReturnAttribute : Attribute
+    {
+        public DoesNotReturnAttribute() { }
+    }
+
+    [AttributeUsageAttribute(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed partial class DoesNotReturnIfAttribute : Attribute
+    {
+        public DoesNotReturnIfAttribute(bool parameterValue) { }
+        public bool ParameterValue { get { throw null; } }
+    }
+
+    [AttributeUsageAttribute(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Constructor | AttributeTargets.Event | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
+    internal sealed partial class ExcludeFromCodeCoverageAttribute : Attribute
+    {
+        public ExcludeFromCodeCoverageAttribute() { }
+    }
+
+    [AttributeUsageAttribute(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false)]
+    internal sealed partial class MaybeNullAttribute : Attribute
+    {
+        public MaybeNullAttribute() { }
+    }
+
+    [AttributeUsageAttribute(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed partial class MaybeNullWhenAttribute : Attribute
+    {
+        public MaybeNullWhenAttribute(bool returnValue) { }
+        public bool ReturnValue { get { throw null; } }
+    }
+
+    [AttributeUsageAttribute(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false)]
+    internal sealed partial class NotNullAttribute : Attribute
+    {
+        public NotNullAttribute() { }
+    }
+
+    [AttributeUsageAttribute(AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, AllowMultiple = true, Inherited = false)]
+    internal sealed partial class NotNullIfNotNullAttribute : Attribute
+    {
+        public NotNullIfNotNullAttribute(string parameterName) { }
+        public string ParameterName { get { throw null; } }
+    }
+
+    [AttributeUsageAttribute(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed partial class NotNullWhenAttribute : Attribute
+    {
+        public NotNullWhenAttribute(bool returnValue) { }
+        public bool ReturnValue { get { throw null; } }
+    }
+
+    //[AttributeUsageAttribute(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    //[ConditionalAttribute("CODE_ANALYSIS")]
+    //internal sealed partial class SuppressMessageAttribute : Attribute
+    //{
+    //    public SuppressMessageAttribute(string category, string checkId) { }
+    //    public string Category { get { throw null; } }
+    //    public string CheckId { get { throw null; } }
+    //    public string Justification { get { throw null; } set { } }
+    //    public string MessageId { get { throw null; } set { } }
+    //    public string Scope { get { throw null; } set { } }
+    //    public string Target { get { throw null; } set { } }
+    //}
+}
+
+#nullable restore
+
+#endif
+
