83d1140d9b766c621eb8fe0647018585326730c9
gitPhate <gabriele.ricci.33.95@gmail.com>
2026-03-11 18:04:01 +0100

    B7: Remove redundant = false initializer on FluidPDFReportOptions.Landscape

--- src/FluidPDF/FluidPDFReportFactory.cs
+++ src/FluidPDF/FluidPDFReportFactory.cs
@@ -88,7 +88,7 @@ namespace FluidPDF
     public sealed class FluidPDFReportOptions
     {
         public PaperFormat Format { get; set; } = PaperFormat.A4;
-        public bool Landscape { get; set; } = false;
+        public bool Landscape { get; set; }
         public MarginOptions MarginOptions { get; set; } = new MarginOptions { Bottom = "0.4 in", Left = "0.4 in", Right = "0.4 in", Top = "0.4 in" };
         public decimal Scale { get; set; } = 1M;
     }
