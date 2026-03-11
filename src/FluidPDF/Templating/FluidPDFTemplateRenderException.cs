117f68939224476212c32fb6f8b24e52021fbe16
gitPhate <gabriele.ricci.33.95@gmail.com>
2026-03-11 17:57:36 +0100

    A1: Make FluidPDFTemplateRenderException public, sealed, primary-constructor style

--- src/FluidPDF/Templating/FluidPDFTemplateRenderException.cs
+++ src/FluidPDF/Templating/FluidPDFTemplateRenderException.cs
@@ -1,19 +1,8 @@
-﻿using System;
+using System;
 
 namespace FluidPDF.Templating
 {
-    internal class FluidPDFTemplateRenderException : Exception
+    public sealed class FluidPDFTemplateRenderException(string message, Exception innerException) : Exception(message, innerException)
     {
-        public FluidPDFTemplateRenderException()
-        {
-        }
-
-        public FluidPDFTemplateRenderException(string message) : base(message)
-        {
-        }
-
-        public FluidPDFTemplateRenderException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
     }
 }
