dfdcd7cd451a47ecde0e98d402e5b360ed7557de
gitPhate <gabriele.ricci.33.95@gmail.com>
2026-03-25 15:26:37 +0100

    improvements to html encoding in scriban

--- src/FluidPDF.Scriban/HTMLEncodedTemplateContext.cs
+++ src/FluidPDF.Scriban/HTMLEncodedTemplateContext.cs
@@ -8,14 +8,13 @@ namespace FluidPDF.Scriban
 {
     internal class HTMLEncodedTemplateContext : TemplateContext
     {
-        public override TemplateContext Write(SourceSpan span, object textAsObject)
+        public override TemplateContext Write(SourceSpan span, object? textAsObject)
             => base.Write(span, EncodeHtml(textAsObject));
 
-        public override ValueTask<TemplateContext> WriteAsync(SourceSpan span, object textAsObject) =>
+        public override ValueTask<TemplateContext> WriteAsync(SourceSpan span, object? textAsObject) =>
             base.WriteAsync(span, EncodeHtml(textAsObject));
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static object EncodeHtml(object textAsObject) => textAsObject is string text ? WebUtility.HtmlEncode(text) : textAsObject;
-
+        private static object? EncodeHtml(object? textAsObject) => textAsObject is not null && textAsObject is string text ? WebUtility.HtmlEncode(text) : textAsObject;
     }
 }
